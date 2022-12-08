4321e7e8f80b17f868a2ae7f379e689a8827790a
NQH1611 <88624093+NQH1611@users.noreply.github.com>
2022-12-08 20:22:59 +0700

    create project

---
+++ QuanLyNhanVien/QuanLyNhanVien/Header.h
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<iomanip>
+#include"string.h"
+using namespace std;
+struct NhanVien
+{
+	char msnv[10];
+	char honv[30];
+	char tennv[30];
+	int luong;
+	int heSoLuong;
+	int thamnien;
+};
+
+typedef struct Node* Nodeptr;
+struct Node
+{
+	NhanVien data;
+	Nodeptr link;
+};
+
+void khoiTao(Nodeptr& list);
+int isEmpty(Nodeptr list);
+void giaiPhong(Nodeptr& list);
+Nodeptr taoNode(NhanVien nv);
+Nodeptr themDau(Nodeptr& list, NhanVien nv);
+Nodeptr themCuoi(Nodeptr& list, NhanVien nv);
---
+++ QuanLyNhanVien/QuanLyNhanVien/Source.cpp
@@ -0,0 +1,52 @@
+﻿#include"Header.h"
+#include<cstring>
+#include<fstream>
+using namespace std;
+
+void khoiTao(Nodeptr& list)
+{
+	list = NULL; //khởi tạo một node có giá trị rỗng trong danh sách liên kết 
+}
+int isEmpty(Nodeptr list)
+{
+	return list == NULL ? 1 : 0;
+}
+void giaiPhong(Nodeptr& list)
+{
+	Nodeptr p = list;
+	while (p != NULL)
+	{
+		list = list->link;
+		delete p;
+		p = list;
+	}
+}
+Nodeptr taoNode(NhanVien nv)
+{
+	Nodeptr p;
+	p = new Node;
+	p->data = nv; //đưa dữ liệu của biến nhân viên nv vào data của node p
+	p->link = NULL; //khởi tạo 1 node p nhưng chưa trỏ đến node nào hết 
+	return p;
+}
+Nodeptr themDau(Nodeptr& list, NhanVien nv)
+{
+	Nodeptr p = taoNode(nv);
+	p->link = list;
+	list = p;
+	return p;
+}
+Nodeptr themCuoi(Nodeptr& list, NhanVien nv)
+{
+	Nodeptr p;
+	p = taoNode(nv);
+	if (list == NULL)
+		list = p;
+	else {
+		Nodeptr q = list;
+		while (q->link != NULL)
+			q = q->link;
+		q->link = p;
+	}
+	return p;
+}
